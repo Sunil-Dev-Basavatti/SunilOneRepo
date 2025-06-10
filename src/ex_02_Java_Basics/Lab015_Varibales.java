14f3077e72822498d3e26714e36c343261325cb7
Sunil-Dev-Basavatti <sunildevbm@gmail.com>
2025-06-10 20:18:15 +0530

    Git Push 2nd day

---
+++ src/ex_02_Java_Basics/Lab015_Varibales.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab015_Varibales {
+    public static void main(String[] args) {
+        byte b=10;
+        System.out.println(b);
+
+        // b ->Variable name ->
+    }
+}
